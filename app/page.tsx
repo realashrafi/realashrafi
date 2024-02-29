389713081f7224aeebd56df7f76bd05afb1fb351
realashrafi <ccvali@outlook.com>
2024-02-29 21:37:59 +0330

    design landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import {GoogleGeminiEffectDemo} from '@/app/components/ui/TestGoogleGeminiEffect'
+import {TracingBeamDemo} from '@/app/components/ui/TestTracingBeam'
+import {LampDemo} from '@/app/components/ui/TestLamp'
 
-export default function Home() {
-  return (
-   <main>
-     Test
-   </main>
-  );
-}
+const Page = () => {
+    return (
+        <div>
+            <GoogleGeminiEffectDemo/>
+            <TracingBeamDemo/>
+            <LampDemo/>
+        </div>
+    );
+};
+
+export default Page;
