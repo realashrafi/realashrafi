b7d577734dc54ccea922432cd407861a60f40d17
ali <zboom0ne01@gmail.com>
2025-09-06 11:05:59 +0330

    update

--- app/components/ui/TabsAbout.tsx
+++ app/components/ui/TabsAbout.tsx
@@ -11,8 +11,86 @@ import {FaSquareGithub} from "react-icons/fa6";
 import {FaLinkedin} from "react-icons/fa";
 import {SummaryJson} from "@/app/store/json";
 import { TextGenerateEffect } from './text-generate-effect';
+import {DraggableCardBody, DraggableCardContainer} from "@/app/components/ui/draggable-card";
 
 function TabsDemo() {
+    const items = [
+        {
+            title: "JS (ES6)",
+            image: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+            className: "absolute top-10 left-[20%] rotate-[-5deg]",
+        },
+        {
+            title: "React.js",
+            image: "https://cloudmatetechnologies.com/wp-content/uploads/2024/06/react.js.png",
+            className: "absolute top-40 left-[25%] rotate-[-7deg]",
+        },
+        {
+            title: "Next.js",
+            image: "https://images.ctfassets.net/23aumh6u8s0i/c04wENP3FnbevwdWzrePs/1e2739fa6d0aa5192cf89599e009da4e/nextjs",
+            className: "absolute top-5 left-[40%] rotate-[8deg]",
+        },
+        {
+            title: "PWA",
+            image: "https://ps.w.org/hyper-pwa/assets/icon-256x256.png?rev=2567651",
+            className: "absolute top-32 left-[55%] rotate-[10deg]",
+        },
+        {
+            title: "Bootstrap",
+            image: "https://getbootstrap.com/docs/5.3/assets/img/bootstrap-icons.png",
+            className: "absolute top-20 right-[35%] rotate-[2deg]",
+        },
+        {
+            title: "Material-UI",
+            image: "https://pbs.twimg.com/profile_images/1798056830041788417/HIapkjDx_400x400.jpg",
+            className: "absolute top-24 left-[45%] rotate-[-7deg]",
+        },
+        {
+            title: "Sass",
+            image: "https://willstyle.co.jp/w/wp-content/uploads/2017/11/sass-2000x1000.jpg",
+            className: "absolute top-8 left-[30%] rotate-[4deg]",
+        },
+        {
+            title: "Tailwind CSS",
+            image: "https://pbs.twimg.com/profile_images/1730334391501488129/G0R0sjHH_400x400.jpg",
+            className: "absolute top-15 left-[35%] rotate-[3deg]",
+        },
+        {
+            title: "TypeScript",
+            image: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
+            className: "absolute top-30 left-[50%] rotate-[-4deg]",
+        },
+        {
+            title: "Git",
+            image: "https://cdn.prod.website-files.com/638106149a6441f0708f5c0a/66c72620456d49ce94e2a2bb_66c72560c6e3b9a31fd0d85b_github-git-cocos-creator.webp",
+            className: "absolute top-12 left-[25%] rotate-[6deg]",
+        },
+        {
+            title: "Redux",
+            image: "https://www.robots-dreams.net/img/entry/Redux.png",
+            className: "absolute top-35 right-[30%] rotate-[-3deg]",
+        },
+        {
+            title: "Context",
+            image: "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/4088805/cover_image/retina_1708x683/cover-react-context-api-312ade466c8ad751079fb421400c4cf9.png",
+            className: "absolute top-18 left-[40%] rotate-[5deg]",
+        },
+        {
+            title: "HTML/CSS",
+            image: "https://www.cse.iitb.ac.in/~ravindramohith/Html-CSS-JS.png.crdownload",
+            className: "absolute top-25 left-[20%] rotate-[-6deg]",
+        },
+        {
+            title: "React Query",
+            image: "https://res.cloudinary.com/dw6wav4jg/image/upload/v1717914209/1_elhu-42TzQEdsFjKDbQhhA_gv2meq.png",
+            className: "absolute top-10 right-[25%] rotate-[4deg]",
+        },
+        {
+            title: "Framer",
+            image: "https://images.seeklogo.com/logo-png/58/1/framer-icon-logo-png_seeklogo-586477.png",
+            className: "absolute top-20 left-[50%] rotate-[-5deg]",
+        },
+    ];
     const skills = [
         {name: "JS (ES6)", icon: <FaJs/>},
         {name: "React.js", icon: <FaReact/>},
@@ -51,22 +129,24 @@ function TabsDemo() {
             value: "skills",
             content: (
                 <div
-                    className="w-[95%] mx-auto xl:w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold bg-cover  text-white bg-[url(https://img2.wallspic.com/crops/4/0/4/2/7/172404/172404-your_name-mitsuha_miyamizu-taki_tachibana-anime-atmosphere-7680x4320.jpg)]">
-                    <p>SKILLS</p>
-                    <div className="lg:p-8 rounded-lg shadow-lg">
-                        <h1 className="text-3xl font-bold text-gray-100 mb-4">Im Working With</h1>
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                            {skills.map((skill, index) => (
-                                <div key={index}
-                                     className="bg-[#00000050] group flex items-center justify-between hover:scale-105 transition-all ease-in p-4 rounded-lg shadow-md overflow-hidden">
-                                    <div className="flex items-center group-hover:text-[#38bdf8] space-x-2">
-                                        <span>{skill.icon}</span>
-                                        <p className="text-lg group-hover:text-[#38bdf8] text-gray-100">{skill.name}</p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+                    className="w-[95%] mx-auto xl:w-full overflow-hidden relative h-full rounded-2xl text-xl md:text-4xl font-bold bg-cover  text-white bg-[url(https://img2.wallspic.com/crops/4/0/4/2/7/172404/172404-your_name-mitsuha_miyamizu-taki_tachibana-anime-atmosphere-7680x4320.jpg)]">
+                    <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
+                        <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl ">
+                            If its your first day at Fight Club, you have to fight.
+                        </p>
+                        {items.map((item,index) => (
+                            <DraggableCardBody key={index} className={item.className}>
+                                <img
+                                    src={item.image}
+                                    alt={item.title}
+                                    className="pointer-events-none relative z-10 h-80 w-80 object-cover"
+                                />
+                                <h3 className="mt-4 text-center text-2xl font-bold text-neutral-700 dark:text-neutral-300">
+                                    {item.title}
+                                </h3>
+                            </DraggableCardBody>
+                        ))}
+                    </DraggableCardContainer>
                 </div>
             ),
         },
